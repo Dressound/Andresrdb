9efdc048bfedbdc029c573119c835e2f2d2a93b3
Andres <Andres@Andres-PC>
2021-01-28 18:44:13 +0100

    andres

---
+++ src/dao/tbpersonaDAO.java
@@ -0,0 +1,31 @@
+
+package dao;
+
+import andresrdb.controler.PersonsJpaController;
+import entity.Persons;
+
+
+/**
+ *
+ * @author Andres
+ */
+public class tbpersonaDAO {
+    
+    private     PersonsJpaController personaController = new PersonsJpaController();
+    
+    private Persons persons = new Persons();
+    
+    public String insertarPersona(){
+        try{
+               
+    }catch (Exception e){
+      }     
+        return null;
+    }
+    
+    public String eliminarPersona(){
+        return null;
+    }
+    
+    
+}
